dd2b045cf270ae8e3d624b8c4a80080145510809
Shaela Jones <shaela.i.jonesmecholsky@nasa.gov>
2024-03-08 19:48:01 -0500

    added squashing factor parsing

--- calc_backbone_metric.py
+++ calc_backbone_metric.py
@@ -31,6 +31,16 @@ prom_scale = 30.
 height_scale = 8.
 min_peak_width = 8.
 
+# squashing factor peak-finding parameters
+mindiff = 0.0001 
+min_series_max = 10.
+prom_scale = 30.
+height_scale = 3
+#height_scale = 2
+#min_peak_width = 2.
+min_peak_width = 1.5
+
+
 
 def angular_separation(phi0, theta0, phi1, theta1, degrees=False, cossep=False):
     """ calculates distance between two points on a sphere, in radians """
@@ -101,6 +111,49 @@ def read_tomo(filename):
     return(tomo_lon, tomo_lat, e_density_map)
 
 
+def locate_ridges(lons, lats, ridgemap):
+
+    ridge_lats=[]
+    ridge_lons=[]
+    # loop over every column of image
+    for lon_ind in range(len(lons)):
+        lon_strip = ridgemap[:,lon_ind]
+        biggest_diff = np.max(abs(np.diff(lon_strip)))
+        if biggest_diff >= mindiff and lon_strip.max() > ridgemap.max() / min_series_max:
+            peaks,_ = find_peaks(lon_strip, prominence = lon_strip.max() / prom_scale, \
+                               height = ridgemap.max() / height_scale, \
+                               width = min_peak_width)
+            ridge_lats.extend([lats[peak] for peak in peaks])
+            ridge_lons.extend([lons[lon_ind] for peak in peaks])
+
+    # loop over every row of image
+    for lat_ind in range(len(lats)):
+        wrap_end = round(10. / 360. * len(lons)) #tack on some extra data so we can find peaks near the edges
+        lat_strip = np.append(ridgemap[lat_ind, :], ridgemap[lat_ind, : wrap_end])
+        biggest_diff = np.max(abs(np.diff(lat_strip)))
+        if biggest_diff >= mindiff and lat_strip.max() > ridgemap.max() / min_series_max:
+             peaks,_ = find_peaks(lat_strip, prominence = lat_strip.max() / prom_scale, \
+                               height = ridgemap.max() / height_scale, width = min_peak_width)
+             peaks[peaks >= len(lons)] -= len(lons)
+             ridge_lats.extend([lats[lat_ind] for peak in peaks])
+             ridge_lons.extend([lons[peak] for peak in peaks])
+
+    # consolidate peaks to grid resolution
+    consolidated_lats = []
+    consolidated_lons = []
+    map_blank = np.zeros((len(lats),len(lons)))
+    grid_res = 360. / (len(lons) + 1)
+    for slat,slon in zip(ridge_lats, ridge_lons):
+        closest_lat = np.argmin([abs(slat - tlat) for tlat in lats])
+        closest_lon = np.argmin([abs(slon - tlon) for tlon in lons])
+        map_blank[closest_lat, closest_lon] = 1
+    nonzeroes = map_blank.nonzero()
+    consolidated_lats = [lats[lat_ind] for lat_ind in nonzeroes[0]]
+    consolidated_lons = [lons[lon_ind] for lon_ind in nonzeroes[1]]
+
+    return consolidated_lats,consolidated_lons
+
+
 def identify_tomography_peaks(tomofile):
     """ identify ridges of higher electron density in tomography-based slices """
 
@@ -109,47 +162,10 @@ def identify_tomography_peaks(tomofile):
     emap = np.asarray(e_density_map)
     emap[emap < 0.] = 0.
 
-    # find peaks in electron density slice
-    streamer_lats=[]
-    streamer_lons=[]
-    # loop over every column of image
-    for lon_ind in range(len(tomo_lon)):
-        lon_strip = emap[:,lon_ind]
-        biggest_diff = np.max(abs(np.diff(lon_strip)))
-        if biggest_diff >= mindiff and lon_strip.max() > emap.max() / min_series_max:
-            peaks,_ = find_peaks(lon_strip, prominence = lon_strip.max() / prom_scale, \
-                               height = emap.max() / height_scale, \
-                               width = min_peak_width)
-            streamer_lats.extend([tomo_lat[peak] for peak in peaks])
-            streamer_lons.extend([tomo_lon[lon_ind] for peak in peaks])
+    # get peak locations
+    density_lats, density_lons = locate_ridges(tomo_lon, tomo_lat, emap)
 
-    # loop over every row of image
-    for lat_ind in range(len(tomo_lat)):
-        wrap_end = round(10. / 360. * len(tomo_lon)) #tack on some extra data so we can find peaks near the edges
-        lat_strip = np.append(emap[lat_ind, :], emap[lat_ind, : wrap_end])
-        biggest_diff = np.max(abs(np.diff(lat_strip)))
-        if biggest_diff >= mindiff and lat_strip.max() > emap.max() / min_series_max:
-             peaks,_ = find_peaks(lat_strip, prominence = lat_strip.max() / prom_scale, \
-                               height = emap.max() / height_scale, width = min_peak_width)
-             peaks[peaks >= len(tomo_lon)] -= len(tomo_lon)
-             streamer_lats.extend([tomo_lat[lat_ind] for peak in peaks])
-             streamer_lons.extend([tomo_lon[peak] for peak in peaks])
-
-
-    # consolidate peaks to grid resolution
-    consolidated_lats = []
-    consolidated_lons = []
-    emap_blank = np.zeros((len(tomo_lat),len(tomo_lon)))
-    grid_res = 360. / (len(tomo_lon) + 1)
-    for slat,slon in zip(streamer_lats, streamer_lons):
-        closest_lat = np.argmin([abs(slat - tlat) for tlat in tomo_lat])
-        closest_lon = np.argmin([abs(slon - tlon) for tlon in tomo_lon])
-        emap_blank[closest_lat, closest_lon] = 1
-    nonzeroes = emap_blank.nonzero()
-    consolidated_lats = [tomo_lat[lat_ind] for lat_ind in nonzeroes[0]]
-    consolidated_lons = [tomo_lon[lon_ind] for lon_ind in nonzeroes[1]]
-
-    return consolidated_lons, consolidated_lats
+    return density_lons, density_lats
 
 
 def combined_figure(cs_xs, cs_ys, streamer_xs, streamer_ys, tomofile, figure_outfile, metric_val):
@@ -181,7 +197,7 @@ def proximity_function(phi, theta, gamma, kappa):
 
     prefactor = kappa / np.sinh(kappa) / 4. / math.pi
     gammadotx = angular_separation(phi, theta, gamma[0], gamma[1], \
-             cossep=True, degrees=True)
+         cossep=True, degrees=True)
     kent_val = prefactor * math.exp(kappa * gammadotx)
     #kent_val = math.exp(kappa*gammadotx)
 
@@ -232,10 +248,41 @@ def calc_backbone_metric(cs_xs, cs_ys, streamer_xs, streamer_ys, test=False):
     return metric
 
 
-def backbone_metric(tomofile, bcbfile, altitude, figure_outfile=False):
+def find_squashingfactor_peaks(wsafile, altitude):
+    """ find peaks in the squashing factor layer of a standard WSA output file:
+           filenames like: wsa_yyyymmddhhmmR<realization>_<observatory>.fits
+    """
+
+    # read wsa output file
+    sfactor,modelheader = fits.getdata(wsafile, header=True)
+    sfactor = np.abs(sfactor[9])
+
+    # print warning about altitude
+    print(f'Warning: User indicated tomographic slice is at {altitude} R_sun.'  
+            f'Squashing factor is measured at {modelheader["RADOUT"]} R_sun.')
+
+    # find peaks
+    nlats = round(180./modelheader['GRID'])
+    grid_radians = np.radians(modelheader['GRID'])
+    lats = np.linspace(-np.pi/2. + 0.5*grid_radians, np.pi/2. - 0.5*grid_radians, nlats)
+    lons = np.linspace(0.5*grid_radians, 2*np.pi - 0.5*grid_radians, 2*nlats)
+    lons = [(i*grid_radians) - (grid_radians)/2. for i in np.arange(1, 2*nlats+1)]
+    sf_lats, sf_lons = locate_ridges(lons, lats, sfactor)
+
+    return sf_lons, sf_lats
+
+
+def backbone_metric(tomofile, modelfile, altitude, figure_outfile=False):
     """ main module """
 
-    cs_xs,cs_ys = find_neutral_line_coords(bcbfile, altitude)
+    if 'bcb' in modelfile:
+        print(f'Running bcb file: {modelfile}.')
+        cs_xs,cs_ys = find_neutral_line_coords(modelfile, altitude)
+    elif 'wsa_' in modelfile:
+        print(f'Running wsa file: {modelfile}.')
+        cs_xs,cs_ys = find_squashingfactor_peaks(modelfile, altitude)
+    else:
+        print(f'Model file: {modelfile} is not in an expected format')
     streamer_xs, streamer_ys = identify_tomography_peaks(tomofile)
     metric = calc_backbone_metric(cs_xs, cs_ys, streamer_xs, streamer_ys)
     if figure_outfile:
@@ -249,8 +296,8 @@ if __name__ == '__main__':
     ARG_PARSER = argparse.ArgumentParser()
     ARG_PARSER.add_argument('-t', '--tomofile', action='store', default='', dest='tomofile')
     ARG_PARSER.add_argument('-a', '--altitude', action='store', default='', dest='altitude', type=float)
-    ARG_PARSER.add_argument('-b', '--bcbfile', action='store', default='', dest='bcbfile')
+    ARG_PARSER.add_argument('-b', '--modelfile', action='store', default='', dest='modelfile')
     ARG_PARSER.add_argument('-fo', '--figure_outfile', action='store', default=False)
     ARGS = ARG_PARSER.parse_args()
 
-    print(backbone_metric(ARGS.tomofile, ARGS.bcbfile, ARGS.altitude, ARGS.figure_outfile))
+    print(backbone_metric(ARGS.tomofile, ARGS.modelfile, ARGS.altitude, ARGS.figure_outfile))
